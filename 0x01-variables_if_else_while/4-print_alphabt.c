69fec77aca58b6e1150d9154fc58aab64e76c4a6
RoviPark27 <qenalib27@gmail.com>
2022-06-13 01:58:40 -0700

    a program that prints the alphabet in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+ * main - displays all lower and upper case alphabets in ascending order
+ * Return: 0 if exited properly, non-zero otherwise
+*/
 int main(void)
 {
-int y;
+	int ch;
 
-for (y = 97; y <= 122; y++)
-{
-<<<<<<< HEAD
-       if (m == 101 || m == 113)
-=======
-if (y == 101 || y == 113)
->>>>>>> c38d008b18676f16774155151031a0fcc1c46752
-{
-}
-else
-{
-putchar(y);
-}
-}
-putchar('\n');
-return (0);
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	if (ch == 'q')
+	{
+	continue;
+	}
+	if (ch == 'e')
+	{
+	continue;
+	}
+	putchar(ch);
+	}
+	putchar(10);
+	return (0);
 }
